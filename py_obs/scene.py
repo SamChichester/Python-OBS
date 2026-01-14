41e5ad40960a07d272bb1402026c0fb642e0db7d
SamChichester <lnsschi1225@gmail.com>
2026-01-14 15:15:37 -0500

    Added source class with example switch_to method.

---
+++ py_obs/scene.py
@@ -0,0 +1,18 @@
+from .source import Source
+
+
+class Scene:
+    def __init__(self, client, name):
+        self._client = client
+        self.name = name
+
+
+    async def switch_to(self):
+        await self._client.request(
+            "SetCurrentProgramScene",
+            {"sceneName": self.name}
+        )
+
+
+    def source(self, name):
+        return Source(self._client, self.name, name)
